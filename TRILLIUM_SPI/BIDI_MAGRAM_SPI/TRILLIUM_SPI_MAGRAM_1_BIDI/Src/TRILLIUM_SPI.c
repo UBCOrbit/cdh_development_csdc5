80565d0022ef907bf1b1ef0332dae613abccb3fe
Tristen Raab <tristenraab@gmail.com>
2019-03-09 16:52:23 -0800

    Bidirectional MAGRAM SPI Communication

---
+++ TRILLIUM_SPI/BIDI_MAGRAM_SPI/TRILLIUM_SPI_MAGRAM_1_BIDI/Src/TRILLIUM_SPI.c
@@ -0,0 +1,127 @@
+#include "TRILLIUM_SPI.h"
+
+UART_HandleTypeDef huart2;
+/**
+Purpose: Switches Pin 8 and 9 from input to output or visa versa depending
+on the passed number
+Accepts: int i, if i = 0 switches pins to input, otherwise switches to output
+**/
+void Switch_IO(int i){
+    uint32_t temp = 0x00U;
+
+    if(i){
+
+           temp = GPIOA->MODER;
+           temp &= ~(GPIO_MODER_MODER0 << (9 * 2U));
+           temp &= ~(GPIO_MODER_MODER0 << (8 * 2U));
+           temp &= ~(GPIO_MODER_MODER0 << (7 * 2U));
+           temp |= ((GPIO_MODE_OUTPUT_PP & GPIO_MODE) << (9 * 2U));
+           temp |= ((GPIO_MODE_OUTPUT_PP & GPIO_MODE) << (8 * 2U));
+           temp |= ((GPIO_MODE_OUTPUT_PP & GPIO_MODE) << (7 * 2U));
+           GPIOA->MODER = temp;
+
+       }else{
+
+   	 	temp = GPIOA->MODER;
+   	 	temp &= ~(GPIO_MODER_MODER0 << (9 * 2U));
+   	 	temp &= ~(GPIO_MODER_MODER0 << (8 * 2U));
+   	 	temp &= ~(GPIO_MODER_MODER0 << (7 * 2U));
+   	 	temp |= ((GPIO_MODE_INPUT & GPIO_MODE) << (9 * 2U));
+   	 	temp |= ((GPIO_MODE_INPUT & GPIO_MODE) << (8 * 2U));
+   	 	temp |= ((GPIO_MODE_INPUT & GPIO_MODE) << (7 * 2U));
+   	 	GPIOA->MODER = temp;
+
+       }
+
+
+}
+
+/**
+Purpose: Sets Pins 8 and 9 low causeing the MCUs connected
+to go into slave mode
+**/
+void Set_Slaves(){
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+
+}
+
+/**
+Purpose: Sets pins 8 and 9 high causing the MCUs connected
+to have the capability to enter into master mode
+**/
+void Set_Masters(){
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+
+}
+
+/**
+Purpose: Clears the MCUs master bit in the register allowing
+it to enter back into master mode
+**/
+void Clear_Master_Register(){
+	READ_REG(hspi2.Instance->SR);
+
+	HAL_Delay(500);
+	uint16_t regCR1 = READ_REG(hspi2.Instance -> CR1);
+	uint8_t count = 0;
+    while((regCR1 & 0x4) == 0){
+    	WRITE_REG(hspi2.Instance -> CR1, ( regCR1| (1 << 2)));
+    	regCR1 = READ_REG(hspi2.Instance -> CR1);
+    	count++;
+    }
+    uint8_t *reg = (uint8_t*) &regCR1;
+    reg[0] += '0';
+    reg[1] += '0';
+    count += '0';
+
+    HAL_UART_Transmit(&huart2, "REG: \n", 6, 0xff);
+    HAL_UART_Transmit(&huart2, &count, 1, 0xff);
+    HAL_UART_Transmit(&huart2, "\n", 2, 0xff);
+}
+
+/**
+ * Purpose: Switches bidirectional SPI
+ * Accepts: int i, if i = 0 switches 2-line unidirectional data mode
+ * if i = 1 switches to 1-line bidirectional data mode
+ */
+void Switch_BIDI(int i){
+
+	uint16_t reg;
+
+	READ_REG(hspi2.Instance->SR);
+
+	uint16_t regCR1 = READ_REG(hspi2.Instance -> CR1);
+
+	if(i){
+		reg = 1 << 15;
+		WRITE_REG(hspi2.Instance -> CR1, ( regCR1| reg));
+		regCR1 = READ_REG(hspi2.Instance -> CR1);
+	}else{
+		reg = 0xff & (0 << 15);
+		WRITE_REG(hspi2.Instance -> CR1, ( regCR1& reg));
+		regCR1 = READ_REG(hspi2.Instance -> CR1);
+	}
+}
+
+/**
+Purpose: Causes the LED on pin 5 to blink x number
+of times with half second delays
+Accepts: An integer x indicating the number of blinks
+**/
+void Blink(int x){
+
+    for(int i = 0; i < x; i ++){
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+
+		HAL_Delay(500);
+
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+		HAL_Delay(500);
+	}
+
+}
---
+++ TRILLIUM_SPI/BIDI_MAGRAM_SPI/TRILLIUM_SPI_MAGRAM_1_BIDI/Src/main.c
@@ -0,0 +1,377 @@
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_hal.h"
+#include "TRILLIUM_SPI.h"
+#include "MRAM.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi2;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+#define GPIO_MODE             0x00000003U
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI2_Init(void);
+static void MX_USART2_UART_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+void print_string(char msg[], int size);
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI2_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+ // uint8_t	mram_status_read, mram_status_write;
+
+  //uint8_t   data_write[] = "Test Data for Memory";
+  //uint8_t   data_read[21];
+  //uint32_t  address;
+
+  // Initialize pins
+ // init_mem();
+  print_string("MRAM Initialized!\n", strlen("MRAM Initialized!\n"));
+
+//  // Enable writing to status register
+//  write_enable(1);
+//
+//  // Write to status register
+//  mram_status_write = 0b01110011;
+//  write_status(&mram_status_write);
+//
+//  // Check if write was executed properly
+//  read_status(&mram_status_read);
+//  if (mram_status_read == mram_status_write)
+//	  print_string("Successful write to status register!\n\n", strlen("Successful write to status register!\n"));
+//  else
+//	  print_string("Failed write to status register\n", strlen("Failed write to status register\n"));
+//
+//  // Write to memory
+//  address = 0x123456;
+//  write_mem(address, 20, data_write);
+//
+//  // Read from memory
+//  read_mem(address, 20, data_read);
+//  int err = 0;
+//  for (int i = 0; i < 20; i++) {
+//	  if (data_read[i] != data_write[i])
+//		  err = 1;
+//  }
+//  if (!err)
+//	  print_string("Successful write to memory!\n", strlen("Successful write to memory!\n"));
+//  else
+//	  print_string("Failed write to memory!\n", strlen("Failed write to memory!\n"));
+  /* USER CODE END SysInit */
+
+  /* Infinite loop */
+  char c = 'q';
+  uint32_t ad = 0x123456;
+
+  Switch_BIDI(0);
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+  /* USER CODE END WHILE */
+	  print_string("Waiting for pin to go low\n", strlen("Waiting for pin to go low\n"));
+
+	  // Wait for pin to go low so this MCU is slave mode
+	  while(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12));
+
+	  print_string("Waiting for pin to go high\n", strlen("Waiting for pin to go high\n"));
+
+	  // Wait for pin to go high, means other MCU is done communicating
+	  while(!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_12));
+
+	  print_string("Switch IO 1\n", strlen("Switch IO 1\n"));
+	  Switch_IO(1);
+	  HAL_Delay(500);
+
+	  print_string("Set slaves\n", strlen("Set slaves\n"));
+	  Set_Slaves();
+	  HAL_Delay(500);
+
+	  print_string("Clear master reg\n", strlen("Clear master reg\n"));
+	  Clear_Master_Register();
+
+	  print_string("Read mem\n", strlen("Read mem\n"));
+	  while(HAL_SPI_Receive(&hspi2, (uint8_t *)&c,1,0xf));
+	  print_string("\n", strlen("\n"));
+	  read_mem(ad, 1, (uint8_t *)&c);
+
+	  print_string((uint8_t *)&c,1);
+	  print_string("\n",1);
+
+	 // Set_Masters();
+	  print_string("Switch IO 0\n", strlen("Switch IO 0\n"));
+	  Switch_IO(0);
+
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+void print_string(char msg[], int size) {
+	HAL_UART_Transmit(&huart2, (uint8_t *)msg, size, 0xFF);
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* SPI2 init function */
+static void MX_SPI2_Init(void)
+{
+
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_HARD_INPUT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/* USART2 init function */
+static void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA7 PA8 PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
